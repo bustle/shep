675608dfdc12d6ab4a5978c5aa20b1e26027c241
Steve Faulkner <southpolesteve@gmail.com>
2016-02-15 16:30:56 -0500

    Skip prompt when name is provided

--- src/run/prompt.js
+++ src/run/prompt.js
@@ -3,6 +3,7 @@ require('babel-register')
 import prompt from '../util/prompt'
 import glob from 'glob'
 import index from './index'
+import { assign } from 'lodash'
 
 export default function(flags){
   const funcs = glob.sync('functions/*').map((path) => path.split('/').pop())
@@ -12,8 +13,10 @@ export default function(flags){
       name: 'name',
       type: 'list',
       choices: funcs,
-      message: 'Which lambda function?'
+      message: 'Which lambda function?',
+      when: () => !flags.name
     }
   ])
+  .then((answers) => assign({},flags,answers))
   .then(index)
 }
