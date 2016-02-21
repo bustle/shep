4d80278b1e4c9643888f56ae5ddf13a1f71f07be
Steve Faulkner <southpolesteve@gmail.com>
2016-02-21 16:12:32 -0500

    Add test for other created files

--- test/new.js
+++ test/new.js
@@ -7,5 +7,9 @@ describe('shepherd new --no-api', ()=>{
 
   it('creates a new directory', () => exists('test-api') )
   it('creates a folder for functions', () => exists('test-api/functions') )
+  it('creates an env file', () => exists('test-api/env.js') )
+  it('creates a package file', () => exists('test-api/package.json') )
+  it('creates a readme', () => exists('test-api/README.md') )
+
 
 })
