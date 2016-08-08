e07fc4c9aec5482de5b0630c92c7e90f826d0846
Steve Faulkner <southpolesteve@gmail.com>
2016-08-08 16:56:39 -0400

    Remove unused modules

--- test/run/index.js
+++ test/run/index.js
@@ -3,9 +3,7 @@ import { create } from '../helpers/fixture'
 import generateFunction from '../../src/generate/function'
 import td from 'testdouble'
 import Promise from 'bluebird'
-import fs from 'fs-extra-promise'
 import run from '../../src/run'
-import build from '../../src/build'
 
 // Supress observetory output
 require('observatory').settings({ write: () => {} })
