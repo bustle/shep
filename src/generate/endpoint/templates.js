cc9e07923b3f11c953daf31bde0bb402aa96e404
Tristan Siegel <Tristan.t.Siegel@gmail.com>
2016-08-15 10:49:50 -0400

    fixes single quotes in mapping template; closes #52 (#53)

--- src/generate/endpoint/templates.js
+++ src/generate/endpoint/templates.js
@@ -2,17 +2,17 @@ export function mapping(){
   return `{
     "headers": {
     #foreach( $key in $input.params().header.keySet() )
-      "$key": "$util.escapeJavaScript($input.params().header.get($key))"#if( $foreach.hasNext ),#end
+      "$key": "$util.escapeJavaScript($input.params().header.get($key)).replaceAll("\\'", "'")"#if( $foreach.hasNext ),#end
     #end
     },
     "pathParameters": {
     #foreach( $key in $input.params().path.keySet() )
-      "$key": "$util.escapeJavaScript($input.params().path.get($key))"#if( $foreach.hasNext ),#end
+      "$key": "$util.escapeJavaScript($input.params().path.get($key)).replaceAll("\\'", "'")"#if( $foreach.hasNext ),#end
     #end
     },
     "queryParameters": {
     #foreach( $key in $input.params().querystring.keySet() )
-      "$key": "$util.escapeJavaScript($input.params().querystring.get($key))"#if( $foreach.hasNext ),#end
+      "$key": "$util.escapeJavaScript($input.params().querystring.get($key)).replaceAll("\\'", "'")"#if( $foreach.hasNext ),#end
     #end
     },
     "body": $input.json('$')
