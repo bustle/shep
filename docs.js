f2126b2611782401abfcf07b1aaba24626852e01
Chris Olszewski <chrisdolszewski@gmail.com>
2016-10-22 15:20:22 -0500

    create docs against cli.js instead of global shep

--- docs.js
+++ docs.js
@@ -4,11 +4,11 @@ const { execSync } = require('child_process')
 
 console.log(`### \`shep\``)
 console.log('```')
-console.log(execSync(`shep --help`).toString().trim())
+console.log(execSync(`./cli.js --help`).toString().replace(/cli\.js/, 'shep').trim())
 console.log('```')
 
 for (var cmd in shep) {
-  let help = execSync(`shep ${cmd} --help`).toString().trim()
+  let help = execSync(`./cli.js ${cmd} --help`).toString().replace(/cli\.js/, 'shep').trim()
   console.log(`#### \`shep ${cmd}\``)
   console.log('```')
   console.log(help)
