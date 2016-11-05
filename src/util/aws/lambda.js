5d7f93446f188eac9e304b34fb3a3631a4851ab2
Chris Olszewski <chrisdolszewski@gmail.com>
2016-11-05 12:35:52 -0500

    fix alias setting to no longer use

--- src/util/aws/lambda.js
+++ src/util/aws/lambda.js
@@ -8,10 +8,16 @@ export function putFunction (config, ZipFile) {
 
   const FunctionName = config.FunctionName
   const Publish = true
+  let functionVersion
 
   return lambda.getFunction({ FunctionName }).promise()
   .then(() => lambda.updateFunctionCode({ ZipFile, FunctionName, Publish }).promise())
+  .tap(({ Version }) => { functionVersion = Version })
   .then(() => lambda.updateFunctionConfiguration(config).promise())
+  .then((func) => {
+    func['Version'] = functionVersion
+    return Promise.resolve(func)
+  })
   .catch({ code: 'ResourceNotFoundException' }, () => {
     const params = merge(config, { Publish, Code: { ZipFile } })
     return lambda.createFunction(params).promise()
