ce7cbbaf41b818d21e5d9d9a2c535cc9962213e5
Chris Olszewski <chrisdolszewski@gmail.com>
2016-11-10 16:04:50 -0600

    fail when generate called without argument

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -3,6 +3,8 @@ export const desc = 'Run `shep generate --help` for additional information'
 export function builder (yargs) {
   return yargs
   .commandDir('./generate')
+  .demand(1)
+  .strict()
 }
 
 export function handler (args) {
