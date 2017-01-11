4f5ad0ceb40cd1df0cb94565bab1e4d14a0240b8
Chris Olszewski <chrisdolszewski@gmail.com>
2017-01-11 16:43:05 -0600

    wrap execa in bluebird

--- src/util/build-functions.js
+++ src/util/build-functions.js
@@ -2,11 +2,8 @@ import exec from './modules/exec'
 
 export default function (PATTERN, NODE_ENV) {
   return exec('webpack', ['--bail'], { env: { PATTERN, NODE_ENV } })
-  .catch((e) => {
-    if (e.code === 'ENOENT') {
-      console.warn('No locally installed webpack found. Verify that webpack is installed')
-    }
-
+  .catch({ code: 'ENOENT' }, (e) => {
+    console.warn('No locally installed webpack found. Verify that webpack is installed')
     throw e
   })
 }
