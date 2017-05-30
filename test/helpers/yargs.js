1d4d83ccaa44a8cbe675ed598f1b8829e2438ee2
Chris Olszewski <chrisdolszewski@gmail.com>
2017-05-30 11:54:29 -0500

    add a bunch of command tests

---
+++ test/helpers/yargs.js
@@ -0,0 +1,26 @@
+function constructCommand (command, flags) {
+  return Object.keys(flags)
+  .map((flag) => {
+    return `--${flag} ${flags[flag]}`
+  })
+  .reduce((str, flag) => {
+    return `${str} ${flag}`
+  }, command)
+}
+
+function allFlags (t, command, parser, flags) {
+  const constructedCommand = constructCommand(command, flags)
+  parser.parse(constructedCommand, (err, argv, output) => {
+    err ? t.fail('Error during yargs validation') : t.pass()
+  })
+}
+allFlags.title = (providedTitle, command) => `No errors when all flags passed to ${command}`
+
+function noFlags (t, command, parser, flags) {
+  parser.parse(command, (err, argv, output) => {
+    err ? t.fail('Error during yargs validation') : t.pass()
+  })
+}
+noFlags.title = (providedTitle, command) => `No errors when no flags passed to ${command}`
+
+export { allFlags, noFlags }
