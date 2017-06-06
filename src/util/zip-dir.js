f9a41ece965b60417e1ccf580285975c93144ff9
Chris Olszewski <chrisdolszewski@gmail.com>
2017-06-06 15:20:55 -0400

    switch from archiver to zipit

--- src/util/zip-dir.js
+++ src/util/zip-dir.js
@@ -1,15 +1,13 @@
-import archiver from 'archiver'
-import streamBuffers from 'stream-buffers'
 import Promise from 'bluebird'
+import Zipit from 'zipit'
 
 export default function (path) {
   return new Promise((resolve, reject) => {
-    const archive = archiver.create('zip')
-    let output = new streamBuffers.WritableStreamBuffer()
-
-    output.on('finish', function () { resolve(output.getContents()) })
-    archive.on('error', function (err) { reject(err) })
-    archive.pipe(output)
-    archive.directory(path, '/').finalize()
+    Zipit({
+      input: path
+    }, (err, buffer) => {
+      if (err) { reject(err) }
+      resolve(buffer)
+    })
   })
 }
